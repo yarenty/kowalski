d03ab35f70d51b7d111898c6f873d21af7999dd2
Jaroslaw Nowosad <yarenty@gmail.com>
2026-03-16 07:25:10 +0000

    wp1

--- kowalski-data-agent/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod agent;
-pub mod config;
-
-pub use agent::DataAgent;
-pub use config::DataAgentConfig;
-
-pub use kowalski_core::config::Config;
-pub use kowalski_core::error::KowalskiError;
-pub use kowalski_core::logging;
